b6458a934bf9e31be1026fdd82c0cf0e41d58341
Sajid Ahmed <sajidahmed@Sajids-MacBook-Pro.local>
2019-11-08 17:36:42 -0500

    Complete level 2

--- MajorProgram3/src/majorprogram3/GameInterface.java
+++ MajorProgram3/src/majorprogram3/GameInterface.java
@@ -5,6 +5,12 @@
  */
 package majorprogram3;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -14,9 +20,25 @@ import javafx.scene.layout.BorderPane;
 public class GameInterface extends BorderPane
 {
     private FightingStage fs;
+    private Fighter fighter1;
+    private Fighter fighter2;
     
     public GameInterface()
     {
+        Rectangle2D viewRectangle2D = new Rectangle2D(100.0, 40.0, 50.0, 90.0);
         fs = new FightingStage(this);
+        fighter1 = new Fighter(fs);
+        fighter2 = new Fighter(fs);
+        try {
+            Image i = new Image(new FileInputStream("Fighter2.png"));
+            fighter2.setImage(i);
+            fighter2.setViewport(viewRectangle2D);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GameInterface.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        fighter1.setX(400);
+        fighter1.setY(600);
+        fighter2.setX(600);
+        fighter2.setY(600);
     }
 }
